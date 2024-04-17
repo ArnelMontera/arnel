19cca18830baf24c21b6c7c370b06fccc1314914
Arnel <164032956+ArnelMontera@users.noreply.github.com>
2024-04-17 19:27:20 +0800

    1st meeting

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+
+include("header.php");
+include("navbar.php");
+
+?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">login</div>
+        <div class="card-body">
+    <div class="form-floating mb-3 mt-3">
+      <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+      <label for="email">Email</label>
+    </div>
+    <div class="form-floating mt-3 mb-3">
+      <input type="text" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+      <label for="pwd">Password</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+             
+        </div>
+        <div class="card-footer">login ver. 1.2.3.5</div>
+    </div>
+
+
+
+<?php
+include("footer.php");
+?>
+
+
+
